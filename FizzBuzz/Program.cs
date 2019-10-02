fa0a832c47ba2ade19452368a6754ffff614c0e0
Tony Liang <liangtony2684@gmail.com>
2019-10-02 11:43:49 -0500

    Summary: rework on code Feature: 	no new feature Bug: 	 no new bug

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -11,7 +11,7 @@ namespace FizzBuzz
             while(count <1000)
             {
                 count++;
-
+/*/
                 if(count%3==0 ){
                     Console.WriteLine("Fizz");
                     countF++;
@@ -19,12 +19,27 @@ namespace FizzBuzz
                 else if(count%5==0){
                     Console.WriteLine("Buzz");
                     countB++;
-                }
+                }*/
                 
-                if( count %3==0 || count%5 ==0){
+                if( count %3==0 &&count%5 ==0){
                     Console.WriteLine("FizzBuzz");
                     countFB++;
-                }else
+                }else if( count %3 ==0 ){
+                     Console.WriteLine("Fizz");
+
+                     countF++;
+
+                }else if (count %5 ==0){
+                     countB++;
+                     Console.WriteLine("Buzz");
+
+                }
+                
+                
+                
+                
+                
+                else
                     Console.WriteLine( "Rest: "+ count);
 
 
