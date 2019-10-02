58d9a166b10b3754b5c30092f56dfa30fa09db9d
Angad Singh Minhas <angadsinghminhas@hotmail.com>
2019-10-02 11:06:30 -0500

    Adding To Branch 'angad' FizzBuzz Program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 1000; i++) {
+                if(i % 3 == 0 &&  i % 5 != 0) {
+                    Console.WriteLine("Fizz | {0}", i);
+                }
+                else if (i % 5 == 0 && i % 3 != 0) {
+                    Console.WriteLine("Buzz | {0}", i);
+                }
+                else if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz | {0}", i);
+                }
+            }
+        }
+    }
+}
