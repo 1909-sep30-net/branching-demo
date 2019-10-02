b7a9f2a6b595b7fd10884543ad599767ded4fb9c
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-02 11:14:49 -0500

    fizzbuzz code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 0;
+            int buzz = 0;
+            int fizzBuzz = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    fizzBuzz += 1;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    fizz += 1;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    buzz += 1;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Fizz {0} Buzz {1} FizzBuzz {2}", fizz, buzz, fizzBuzz);
+
+        }
+    }
+}
