6f8da80b2a3c7f71bcdc67dac93fcf56b12e063a
Girard Sanford <girard.sanford@gmail.com>
2019-10-02 11:37:23 -0500

    fizzbuzz code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizzCnt = 0;
+            int buzzCnt = 0;
+            int fizzbuzzCnt = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if ((i % 3 == 0) & (i % 5 != 0))
+                {
+                    Console.WriteLine("Fizz");
+                    fizzCnt++;
+                }
+                else if ((i % 5 == 0) & (i % 3 != 0))
+                {
+                    Console.WriteLine("Buzz");
+                    buzzCnt++;
+                }
+                else if ((i % 3 == 0) & (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                    fizzbuzzCnt++;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            System.Console.WriteLine("Fizz= " + fizzCnt);
+            System.Console.WriteLine("Buzz= " + buzzCnt);
+            System.Console.WriteLine("FizzBuzz= " + fizzbuzzCnt);
+        }
+    }
+}
