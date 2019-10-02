af88f1814b08de8128cf4f713a9d1b0bd8f6c286
Alvert18 <albertoace_18@outlook.com>
2019-10-02 11:31:23 -0500

    added gitignore and FizzBuzz program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz_num = 0;
+            int buzz_num = 0;
+            int fizzbuzz_num = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if(i%3 == 0 && i%5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                    fizz_num++;
+                }
+                else if(i%5 == 0 && i%3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                    buzz_num++;
+                }
+                else if(i%5 == 0 && i%3 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                    fizzbuzz_num++;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Amount of \"Fizz\": " + fizz_num);
+            Console.WriteLine("Amount of \"Buzz\": " + buzz_num);
+            Console.WriteLine("Amount of \"FizzBuzz\": " + fizzbuzz_num);
+        }
+    }
+}
