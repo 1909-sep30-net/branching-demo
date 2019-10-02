5b7a950e82d810cc540b0a28c6551ec543f7e102
mariellenolasco <mariellenolasco@gmail.com>
2019-10-02 11:29:37 -0500

    This is my fizzbuzz code

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<1001; i++){
+                if(i%15 == 0){
+                    Console.WriteLine("Fizzbuzz");
+                    
+                } else if(i%3 == 0){
+                    Console.WriteLine("Fizz");
+                    
+                } else if (i%5 == 0){
+                    Console.WriteLine("Buzz");
+                    
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Total number of Fizz: " + (1000/3 - 1000/15));
+            Console.WriteLine("Total number of Buzz: " + (1000/5 - 1000/15));
+            Console.WriteLine("Total number of FizzBuzz: " + 1000/15);
+        }
+    }
+}
