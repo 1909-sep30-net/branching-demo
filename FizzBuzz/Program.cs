75edfaba6c08528d699ca90f27794587d15ce99d
Colton Clary <coltonclary0806@yahoo.com>
2019-10-02 11:35:28 -0500

    added FizzBuzz assignment

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=1000;i++)
+            {
+                if(i%3 == 0 & i%5>1)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+               else if(i%5==0 & i%3>1)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+                else if(i%5==0 & i%3==0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                    
+            }
+        }
+    }
+}
