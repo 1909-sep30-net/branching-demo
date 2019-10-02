4e57e6a6e6b7d5c0781559684ee4c47a2dc84f45
kmanrulze <kcguzman@comcast.net>
2019-10-02 10:28:44 -0600

    Fizzes have been fizzed, and buzzes have been buzzed!

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizzes = 0;
+            int buzzes = 0;
+            int fizzbuzzes = 0;
+            
+            for(int i=0;i<1001;i++)
+            {
+                if(i%3 == 0 && i%5 == 0)
+                {
+                    System.Console.WriteLine(i + " ------- FizzBuzz");
+                    fizzbuzzes++;
+                }
+                else if (i%3==0)
+                {
+                    System.Console.WriteLine(i + " ------- Fizz");
+                    fizzes++;
+                }
+                else if (i%5==0)
+                {
+                    System.Console.WriteLine(i + " ------- Buzz");
+                    buzzes++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+            System.Console.WriteLine("Number of Fizzes: " + fizzes.ToString() + "\nNumber of Buzzes: " + buzzes.ToString() + "\nNumber of FizzBuzzes: " + fizzbuzzes.ToString());
+        }
+    }
+}
