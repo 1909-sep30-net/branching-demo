065545c2e17aa5d58b1ad6aa0f24f88a18cbed82
Tony Liang <liangtony2684@gmail.com>
2019-10-02 11:30:45 -0500

    Summary: nothing new

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count =0, countF=0, countB=0, countFB=0;
+            
+            while(count <1000)
+            {
+                count++;
+
+                if(count%3==0 ){
+                    Console.WriteLine("Fizz");
+                    countF++;
+                }
+                else if(count%5==0){
+                    Console.WriteLine("Buzz");
+                    countB++;
+                }
+                
+                if( count %3==0 || count%5 ==0){
+                    Console.WriteLine("FizzBuzz");
+                    countFB++;
+                }else
+                    Console.WriteLine( "Rest: "+ count);
+
+
+                
+            }
+
+            Console.WriteLine(" Fizz Count: "+ countF+" Buzz Count: "+ countB+" FizzBuzz Count:"+ countFB );
+
+        }
+    }
+}
