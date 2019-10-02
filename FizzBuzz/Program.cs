72551af90d8feda5375eca3eedf3034fa868a587
Javon Negahban <javon.negahban@cox.net>
2019-10-02 11:20:26 -0500

    Added Fizzbuzz project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizzbuzz = 0;
+            int buzz = 0;
+            int fizz = 0;
+
+            for (int i = 1; i <= 1000; i++) {
+                if (i % 15 == 0) {
+                    Console.WriteLine("Fizzbuzz");
+                    fizzbuzz++;
+                } else if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                    buzz++;
+                } else if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                    fizz++;
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("The number of fizz is: " + fizz);
+            Console.WriteLine("The number of buzz is: " + buzz);
+            Console.WriteLine("The number of fizzbuzz is: " + fizzbuzz);
+        }
+    }
+}
