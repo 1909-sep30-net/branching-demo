af054d8066a8a7aa669cc72f77a254d5d90ba82d
K. Zhou <kaichengzhou1@gmail.com>
2019-10-02 09:51:58 -0700

    add .gitignore.... changed FizzBuzz method

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,26 +6,34 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            FizzBuzz(1000);
+        }
+        static void FizzBuzz(int number)
+        {
             int fizz = 0;
             int buzz = 0;
             int fizzbuzz = 0;
-            for(int i=1;i<=1000;i++){
-                if(i%3 == 0 && i%5 != 0 ){
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
                     fizz++;
                     Console.WriteLine("Fizz");
                 }
-                else if (i%5 == 0 && i%3 != 0 ){
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
                     buzz++;
                     Console.WriteLine("Buzz");
                 }
-                else if (i%3 == 0 && i%5 == 0 ){
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
                     fizzbuzz++;
                     Console.WriteLine("FizzBuzz");
                 }
                 else
                     Console.WriteLine(i);
             }
-            Console.WriteLine("We have " + fizz+" Fizz, " + buzz+ " Buzz, and "+ fizzbuzz+ " FizzBuzz");
+            Console.WriteLine("We have " + fizz + " Fizz, " + buzz + " Buzz, and " + fizzbuzz + " FizzBuzz");
         }
     }
 }
