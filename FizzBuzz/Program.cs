18da0870500262497296d3c9cf0b50eaaeb06c73
Angad Singh Minhas <angadsinghminhas@hotmail.com>
2019-10-02 11:31:05 -0500

    Addition of Number Of Occurances of Each Term in the Output

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,17 +6,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
+            int sumFizz = 0, sumBuzz = 0, sumRest = 0;
             for(int i = 1; i <= 1000; i++) {
                 if(i % 3 == 0 &&  i % 5 != 0) {
                     Console.WriteLine("Fizz | {0}", i);
+                    sumFizz++;
                 }
                 else if (i % 5 == 0 && i % 3 != 0) {
                     Console.WriteLine("Buzz | {0}", i);
+                    sumBuzz++;
                 }
                 else if (i % 3 == 0 && i % 5 == 0) {
                     Console.WriteLine("FizzBuzz | {0}", i);
+                    sumRest++;
                 }
             }
+            Console.WriteLine("Number Of Times Fizz Occured: {0}", sumFizz);
+            Console.WriteLine("Number Of Times Buzz Occured: {0}", sumBuzz);
+            Console.WriteLine("Number Of Times Neither Occured Occured: {0}", sumRest);
         }
     }
 }
