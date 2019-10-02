48ea7ecf769f56d98cad7fb3feecc40a8290ddd7
K. Zhou <kaichengzhou1@gmail.com>
2019-10-02 09:12:11 -0700

    simple fizzbuzz application

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 0;
+            int buzz = 0;
+            int fizzbuzz = 0;
+            for(int i=1;i<=1000;i++){
+                if(i%3 == 0 && i%5 != 0 ){
+                    fizz++;
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5 == 0 && i%3 != 0 ){
+                    buzz++;
+                    Console.WriteLine("Buzz");
+                }
+                else if (i%3 == 0 && i%5 == 0 ){
+                    fizzbuzz++;
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                    Console.WriteLine(i);
+            }
+            Console.WriteLine("We have " + fizz+" Fizz, " + buzz+ " Buzz, and "+ fizzbuzz+ " FizzBuzz");
+        }
+    }
+}
